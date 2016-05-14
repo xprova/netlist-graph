7963484c834b8017ef484fcf4eb7226137a023d9
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-05-15 00:59:45 +0100

    Fix accidental overwrite of multibit.v

--- src/test/resources/multibit.v
+++ src/test/resources/multibit.v
@@ -1,9 +1,12 @@
-module main(clk, rst, x, y);
+module counter(clk, rst, ena, count);
 
-	input clk, rst, x;
+	input clk, rst, ena;
 
-	output y;
+	output [3:0] count;
 
-	NOT inv1(x, y);
+	NOT inv0 (ena, count[0]);
+	NOT inv1 (ena, count[1]);
+	NOT inv2 (ena, count[2]);
+	NOT inv3 (ena, count[3]);
 
 endmodule
