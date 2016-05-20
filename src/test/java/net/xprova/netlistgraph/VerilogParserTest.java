d668b2e88a430f25d54bbb6fda1da72fa590c14c
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-05-21 00:15:47 +0100

    Replace assert with assertEquals

--- src/test/java/net/xprova/netlistgraph/VerilogParserTest.java
+++ src/test/java/net/xprova/netlistgraph/VerilogParserTest.java
@@ -25,41 +25,41 @@ public class VerilogParserTest extends TestCase {
 		String fullPath2 = classLoader.getResource("minimal.v").getPath();
 		ArrayList<Netlist> netListArr = VerilogParser.parseFile(fullPath2, simpleLib);
 
-		assert (netListArr.size() == 1);
+		assertEquals(netListArr.size(), 1);
 
 		Netlist nl = netListArr.get(0);
 
 		// test ports
-		assert (nl.ports.size() == 4);
-		assert (nl.ports.get("x").direction == PinDirection.IN);
-		assert (nl.ports.get("clk").direction == PinDirection.IN);
-		assert (nl.ports.get("rst").direction == PinDirection.IN);
-		assert (nl.ports.get("y").direction == PinDirection.OUT);
+		assertEquals(nl.ports.size(), 4);
+		assertEquals(nl.ports.get("x").direction, PinDirection.IN);
+		assertEquals(nl.ports.get("clk").direction, PinDirection.IN);
+		assertEquals(nl.ports.get("rst").direction, PinDirection.IN);
+		assertEquals(nl.ports.get("y").direction, PinDirection.OUT);
 
 		// test modules
-		assert (nl.modules.size() == 1);
-		assert (nl.modules.get("inv1").type.equals("NOT"));
-		assert (nl.modules.get("inv1").id.equals("inv1"));
+		assertEquals(nl.modules.size(), 1);
+		assertEquals(nl.modules.get("inv1").type, "NOT");
+		assertEquals(nl.modules.get("inv1").id, "inv1");
 
 		// test nets
-		assert (nl.nets.size() == 4);
-		assert (nl.nets.get("clk").id.equals("clk"));
-		assert (nl.nets.get("rst").id.equals("rst"));
-		assert (nl.nets.get("x").id.equals("x"));
-		assert (nl.nets.get("y").id.equals("y"));
+		assertEquals(nl.nets.size(), 4);
+		assertEquals(nl.nets.get("clk").id, "clk");
+		assertEquals(nl.nets.get("rst").id, "rst");
+		assertEquals(nl.nets.get("x").id, "x");
+		assertEquals(nl.nets.get("y").id, "y");
 
 		// test module connections
 		HashMap<String, PinConnection> inv1Cons = nl.modules.get("inv1").connections;
-		assert (inv1Cons.size() == 2);
-		assert (inv1Cons.get("a").dir == PinDirection.IN);
-		assert (inv1Cons.get("y").dir == PinDirection.OUT);
-		assert (inv1Cons.get("a").net.equals("x"));
-		assert (inv1Cons.get("y").net.equals("y"));
-		assert (inv1Cons.get("a").bit == 0);
-		assert (inv1Cons.get("y").bit == 0);
+		assertEquals(inv1Cons.size() , 2);
+		assertEquals(inv1Cons.get("a").dir, PinDirection.IN);
+		assertEquals(inv1Cons.get("y").dir, PinDirection.OUT);
+		assertEquals(inv1Cons.get("a").net, "x");
+		assertEquals(inv1Cons.get("y").net, "y");
+		assertEquals(inv1Cons.get("a").bit, 0);
+		assertEquals(inv1Cons.get("y").bit, 0);
 
 		// test netlist name
-		assert (nl.name.equals("main"));
+		assertEquals(nl.name, "main");
 
 	}
 
@@ -76,17 +76,17 @@ public class VerilogParserTest extends TestCase {
 		String fullPath2 = classLoader.getResource("multibit.v").getPath();
 		ArrayList<Netlist> netListArr = VerilogParser.parseFile(fullPath2, simpleLib);
 
-		assert (netListArr.size() == 1);
+		assertEquals(netListArr.size(), 1);
 
 		Netlist nl = netListArr.get(0);
 
 		// test multi-bit port
-		assert (nl.nets.size() == 4);
-		assert (nl.nets.get("count").start == 3);
-		assert (nl.nets.get("count").end == 0);
-		assert (nl.nets.get("count").getCount() == 4);
-		assert (nl.nets.get("count").getHigher() == 3);
-		assert (nl.nets.get("count").getLower() == 0);
+		assertEquals(nl.nets.size(), 4);
+		assertEquals(nl.nets.get("count").start, 3);
+		assertEquals(nl.nets.get("count").end, 0);
+		assertEquals(nl.nets.get("count").getCount(), 4);
+		assertEquals(nl.nets.get("count").getHigher(), 3);
+		assertEquals(nl.nets.get("count").getLower(), 0);
 
 	}
 
