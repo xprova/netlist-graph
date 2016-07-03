40b189b746e032185061e16bdf7a63a68e7448e8
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-07-03 14:52:42 +0100

    Add MultiMap methods to get keys, values and entries

--- src/main/java/net/xprova/graph/MultiMap.java
+++ src/main/java/net/xprova/graph/MultiMap.java
@@ -1,6 +1,8 @@
 package net.xprova.graph;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
 
 public class MultiMap<K1, K2, V> {
 
@@ -26,6 +28,12 @@ public class MultiMap<K1, K2, V> {
 
 	}
 
+	public void put(Pair<K1, K2> key, V value) {
+
+		put(key.first, key.second, value);
+
+	}
+
 	public V get(K1 key1, K2 key2) {
 
 		HashMap<K2, V> subMap = map.get(key1);
@@ -36,6 +44,12 @@ public class MultiMap<K1, K2, V> {
 		}
 
 		return subMap.get(key2);
+
+	}
+
+	public V get(Pair<K1, K2> key) {
+
+		return get(key.first, key.second);
 
 	}
 
@@ -51,11 +65,90 @@ public class MultiMap<K1, K2, V> {
 		return subMap.containsKey(key2);
 	}
 
+	public boolean contains(Pair<K1, K2> key) {
+
+		return contains(key.first, key.second);
+
+	}
+
 	public V remove(K1 key1, K2 key2) {
 
 		HashMap<K2, V> subMap = map.get(key1);
 
 		return (subMap == null) ? null : subMap.remove(key2);
+	}
+
+	public V remove(Pair<K1, K2> key) {
+
+		return remove(key.first, key.second);
+
+	}
+
+	public HashSet<Pair<K1, K2>> getKeys() {
+
+		HashSet<Pair<K1, K2>> keys = new HashSet<Pair<K1, K2>>();
+
+		for (Entry<K1, HashMap<K2, V>> entry1 : map.entrySet()) {
+
+			K1 key1 = entry1.getKey();
+
+			for (K2 key2 : entry1.getValue().keySet()) {
+
+				keys.add(new Pair<K1, K2>(key1, key2));
+
+			}
+
+		}
+
+		return keys;
+
+	}
+
+	public HashSet<V> getValues() {
+
+		HashSet<V> values = new HashSet<V>();
+
+		for (HashMap<K2, V> map2 : map.values()) {
+
+			for (V val : map2.values())
+				values.add(val);
+
+		}
+
+		return values;
+
+	}
+
+	public HashSet<Pair<Pair<K1, K2>, V>> entrySet() {
+
+		// HashSet<Pair<K1, K2>> keys = new HashSet<Pair<K1, K2>>();
+
+		HashSet<Pair<Pair<K1, K2>, V>> result = new HashSet<Pair<Pair<K1, K2>, V>>();
+
+		for (Entry<K1, HashMap<K2, V>> entry1 : map.entrySet()) {
+
+			K1 key1 = entry1.getKey();
+
+			for (Entry<K2, V> entry2 : entry1.getValue().entrySet()) {
+
+				// keys.add(new Pair<K1, K2>(key1, key2));
+
+				K2 key2 = entry2.getKey();
+
+				V val = entry2.getValue();
+
+				Pair<K1, K2> p1 = new Pair<K1, K2>(key1, key2);
+
+				Pair<Pair<K1, K2>, V> p2 = new Pair<Pair<K1, K2>, V>(p1, val);
+
+				result.add(p2);
+
+			}
+
+		}
+
+		return result;
+
 	}
 
 }
---
+++ src/main/java/net/xprova/graph/Pair.java
@@ -0,0 +1,15 @@
+package net.xprova.graph;
+
+public class Pair<V1, V2> {
+
+	public final V1 first;
+	public final V2 second;
+
+	public Pair(V1 first, V2 second) {
+
+		this.first = first;
+		this.second = second;
+
+	}
+
+}
